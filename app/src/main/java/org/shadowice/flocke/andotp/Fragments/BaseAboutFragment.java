4ff3538c012dd0ce5833ad942d16d4a6d230d359
Vascomax <65168100+Vascomax@users.noreply.github.com>
2021-06-08 06:29:06 +0200

    Link update

--- app/src/main/java/org/shadowice/flocke/andotp/Fragments/BaseAboutFragment.java
+++ app/src/main/java/org/shadowice/flocke/andotp/Fragments/BaseAboutFragment.java
@@ -28,6 +28,7 @@ import org.shadowice.flocke.andotp.Utilities.Tools;
 public class BaseAboutFragment extends Fragment {
     private static final String GITHUB_URI = "https://github.com/Vascomax/andOTP";
     private static final String GITHUB_URI_ORIG = "https://github.com/andOTP/andOTP";
+    private static final String SOURCE_URI = GITHUB_URI + "/tree/GalaxyVersionMaster";
     private static final String CHANGELOG_URI = GITHUB_URI + "/blob/GalaxyVersionMaster/CHANGELOG.md";
     private static final String MIT_URI = GITHUB_URI + "/blob/GalaxyVersionMaster/LICENSE.txt";
     private static final String FAQ_URI = GITHUB_URI_ORIG + "/wiki/Frequently-Asked-Questions";
@@ -131,7 +132,7 @@ public class BaseAboutFragment extends Fragment {
         source.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                openURI(GITHUB_URI);
+                openURI(SOURCE_URI);
             }
         });
         faq.setOnClickListener((View view) -> openURI(FAQ_URI));
